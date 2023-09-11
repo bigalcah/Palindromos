d105adc81cd1ee1dcaafbbcb5a27f9b989eb58c0
bigalcah <142508383+bigalcah@users.noreply.github.com>
2023-09-11 17:17:51 -0300

    Update Main.java
    final final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,8 @@ public class Main {
             if(cadena.isEmpty()){
                 throw new IllegalArgumentException("La cadena está vacía");
             }
-           // el \\ no me quedó tan claro la función que cumple pero es algo así como omitir los caracteres especiales, incluyendo el propio \ y s+ se refiere a uno o mas espacios varios (los espacios vacios se son s)
+           /* el \\ no me quedó tan claro la función que cumple pero es algo así como omitir los caracteres especiales, incluyendo el propio \ 
+            y s+ se refiere a uno o mas espacios varios (los espacios vacios se son s, el siguiente parametro es el reemplazo, en este caso vacio*/
             char[] cadenaArreglo = cadena.replaceAll("\\s+", "").toLowerCase().toCharArray();
             char[] resultado = new char[cadenaArreglo.length];
 
